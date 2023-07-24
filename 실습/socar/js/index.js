659dece9312072846b0c86617a55e845322abd25
venty1993 <75829033+venty1993@users.noreply.github.com>
2023-07-24 12:58:50 +0900

    asd

--- 실습/socar/js/index.js
+++ 실습/socar/js/index.js
@@ -35,3 +35,21 @@ window.addEventListener('scroll', () => {
         incline.style.left = '0%';
     }
 })
+
+
+
+
+const hambergerButton = document.querySelector('.hamberger');
+const hambergerClick = document.querySelector('.hamberger-click');
+const hambergerNav = document.querySelector('.hamberger-nav')
+
+
+hambergerButton.addEventListener('click', () => {
+    hambergerButton.classList.add('active')
+    hambergerClick.classList.add('active');
+
+    setTimeout(() => {
+        hambergerNav.style.transform = `none`;
+    }, 1);
+
+})
